0f3e67900c5d45b816a5438eec66d8acf8e27f07
julvernois <julvernois@users.noreply.github.com>
2016-09-05 12:02:26 +0200

    Update RegexParser.php

--- src/Parser/RegexParser.php
+++ src/Parser/RegexParser.php
@@ -38,7 +38,7 @@ final class RegexParser implements ParserInterface
         // loop instead of array_map to pass the arguments explicitly
         $shortcodes = array();
         foreach($matches[0] as $match) {
-            $offset = mb_strlen(substr($text, 0, $match[1]), 'utf-8');
+            $offset = mb_strlen(substr($text, 0, $match[1]));
             $shortcodes[] = $this->parseSingle($match[0], $offset);
         }
 
